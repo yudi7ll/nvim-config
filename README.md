fd86e879dc8db94c04674f1a9c30f7f3ef7a54f6
Yudi <madewahyudi22@gmail.com>
2021-03-26 16:27:24 +0800

    update readme & update mapping for lazygit

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - [Neovim Nightly ( >= 0.5.0 )](https://github.com/neovim/neovim/wiki/Installing-Neovim)
 - [FZF](https://github.com/junegunn/fzf)
 - [the_silver_searcher](https://github.com/ggreer/the_silver_searcher)
-- [Tree Sitter (Optional)](https://github.com/tree-sitter/tree-sitter/blob/master/cli/README.md)
-
+- [Lazygit (Optional)](https://github.com/jesseduffield/lazygit)
+- [Lazydocker (Optional)](https://github.com/jesseduffield/lazydocker)
--- lua/modules/floaterm/init.lua
+++ lua/modules/floaterm/init.lua
@@ -5,6 +5,6 @@ vim.g.floaterm_keymap_next   = '<leader>tj'
 vim.g.floaterm_keymap_toggle = '<leader>T'
 
 -- Lazygit
-vim.api.nvim_set_keymap('n', '<leader>tl', ':FloatermNew --width=0.9 --height=0.9 --name=lazygit --autoclose=2 lazygit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tg', ':FloatermNew --width=0.9 --height=0.9 --name=lazygit --autoclose=2 lazygit<CR>', { noremap = true, silent = true })
 -- lazydocker
 vim.api.nvim_set_keymap('n', '<leader>td', ':FloatermNew --width=0.9 --height=0.9 --name=lazydocker --autoclose=2 lazydocker<CR>', { noremap = true, silent = true })
