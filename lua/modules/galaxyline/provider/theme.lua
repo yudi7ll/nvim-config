a37cb966e9cc0540b102c670b21d2f8a09c95715
Yudi <madewahyudi22@gmail.com>
2021-03-28 18:03:54 +0800

    update galaxyline theme

---
+++ lua/modules/galaxyline/provider/theme.lua
@@ -0,0 +1,28 @@
+local colors = {}
+
+colors.default      = {
+   bg            = '#282a36',
+   fg            = '#f8f8f2',
+   section_bg    = '#38393f',
+   Brown         = '#905532',
+   Aqua          = '#3AFFDB',
+   Blue          = '#689FB6',
+   Cyan          = '#8be9fd',
+   Darkblue      = '#44788E',
+   Purple        = '#834F79',
+   Red           = '#ff5555',
+   Beige         = '#F5C06F',
+   Yellow        = '#f1fa8c',
+   DarkYellow    = '#D7BA7D',
+   Orange        = '#D4843E',
+   Darkorange    = '#F16529',
+   Pink          = '#CB6F6F',
+   Magenta       = '#ff79c6',
+   Salmon        = '#EE6E73',
+   Green         = '#8FAA54',
+   Lightgreen    = '#31B53E',
+   White         = '#FFFFFF',
+   LightBlue     = '#5fd7ff',
+}
+
+return colors
