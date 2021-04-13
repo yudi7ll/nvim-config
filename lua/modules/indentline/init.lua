5a4a5bf2219719be15e4b98700f7746b2a9654c5
Yudi <madewahyudi22@gmail.com>
2021-04-13 08:43:32 +0800

    add floaterm to indentLine_fileTypeExclude

--- lua/modules/indentline/init.lua
+++ lua/modules/indentline/init.lua
@@ -1,3 +1,3 @@
 vim.g.indentLine_char            = '▏'
 vim.g.indentLine_faster          = 1
-vim.g.indentLine_fileTypeExclude = { 'text', 'markdown', 'help', 'json' }
+vim.g.indentLine_fileTypeExclude = { 'text', 'markdown', 'help', 'floaterm' }
