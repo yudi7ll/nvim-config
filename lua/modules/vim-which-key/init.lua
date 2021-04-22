f076495ce90a446b95e7ccecd9ccdb8c93b4b581
Yudi <madewahyudi22@gmail.com>
2021-04-22 19:30:33 +0800

    update vim-which-key config

--- lua/modules/vim-which-key/init.lua
+++ lua/modules/vim-which-key/init.lua
@@ -1,11 +1,19 @@
 local wk = require('whichkey_setup')
 
+vim.g.which_key_timeout = 100
+
 vim.api.nvim_set_keymap('n', '<leader>', ":WhichKey '<Space>'<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<localleader>', ":WhichKey ','<CR>", { noremap = true, silent = true })
 
 local keymap = {
 	a = {
 		name = '+CocAction',
+	},
+	h = {
+		name = '+Git',
+	},
+	c = {
+		name = '+COC'
 	}
 }
 
