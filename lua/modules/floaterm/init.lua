e4ba647669e9d60caa65b4810966ff32ee8c3c2d
Yudi <madewahyudi22@gmail.com>
2021-06-11 18:52:38 +0800

    add next & prev keymap for floaterm plugin

--- lua/modules/floaterm/init.lua
+++ lua/modules/floaterm/init.lua
@@ -1,12 +1,17 @@
+local nmap = require('common.utils').nmap
+
 vim.g.floaterm_keymap_toggle = '<silent><localleader>T'
-vim.g.floaterm_width = 0.9
-vim.g.floaterm_height = 0.9
+vim.g.floaterm_keymap_new    = '<F7>'
+vim.g.floaterm_keymap_prev   = '<F8>'
+vim.g.floaterm_keymap_next   = '<F9>'
+vim.g.floaterm_width         = 0.9
+vim.g.floaterm_height        = 0.9
 
 vim.api.nvim_command('hi FloatermBorder guibg=nil')
 
 -- Lazygit
-vim.api.nvim_set_keymap('n', '<localleader>tg', ':FloatermNew --name=lazygit --autoclose=2 lazygit<CR>', { noremap = true, silent = true })
+nmap('<localleader>tg', ':FloatermNew --name=lazygit --autoclose=2 lazygit<CR>', { noremap = true, silent = true })
 -- lazydocker
-vim.api.nvim_set_keymap('n', '<localleader>td', ':FloatermNew --name=lazydocker --autoclose=2 lazydocker<CR>', { noremap = true, silent = true })
+nmap('<localleader>td', ':FloatermNew --name=lazydocker --autoclose=2 lazydocker<CR>', { noremap = true, silent = true })
 -- ncdu
-vim.api.nvim_set_keymap('n', '<localleader>tu', ':FloatermNew --name=lazydocker --autoclose=2 ncdu<CR>', { noremap = true, silent = true })
+nmap('<localleader>tu', ':FloatermNew --name=lazydocker --autoclose=2 ncdu<CR>', { noremap = true, silent = true })
