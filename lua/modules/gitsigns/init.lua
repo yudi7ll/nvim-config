0fb1a7fdf51819e1c093d6dc5036e157f7680e2a
Yudi <madewahyudi22@gmail.com>
2021-09-05 22:16:50 +0800

    remove "use_decoration_api" from gitsigns config

--- lua/modules/gitsigns/init.lua
+++ lua/modules/gitsigns/init.lua
@@ -17,8 +17,7 @@ require("gitsigns").setup {
         ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
         ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
-    },
-    use_decoration_api = false
+    }
 }
 
 local cmd = vim.cmd
