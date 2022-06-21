a9bbd7138b68b0eb7e1c45cad0cc0fdc57922ecb
Yudi <madewahyudi22@gmail.com>
2022-06-21 18:52:52 +0800

    update vim blade config

--- lua/modules/vim-blade/init.lua
+++ lua/modules/vim-blade/init.lua
@@ -1,6 +1,7 @@
-vim.g.blade_custom_directives = { 'csrf', 'method', 'props', 'stack' }
+vim.g.blade_custom_directives = { "csrf", "method", "props", "stack", "livewireStyles", "livewireScripts" }
 vim.g.blade_custom_directives_pairs = {
-  error = 'enderror',
-  push = 'endpush',
-  foreach = 'endforeach'
+	error = "enderror",
+	push = "endpush",
+	foreach = "endforeach",
+	livewire = "livewire",
 }
