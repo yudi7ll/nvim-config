90261101b53eb1efa88d71d59e3f6899c80dc364
Yudi <madewahyudi22@gmail.com>
2022-07-26 23:31:22 +0800

    minor

--- lua/modules/bufferline/init.lua
+++ lua/modules/bufferline/init.lua
@@ -3,6 +3,12 @@ local nmap = utils.nmap
 
 require("bufferline").setup({
 	options = {
+		-- diagnostics = "coc", -- sometime the error won't dissappear
+		-- diagnostics_update_in_insert = true,
+		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		-- 	local icon = level:match("error") and " " or " "
+		-- 	return " " .. icon .. count
+		-- end,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		show_tab_indicators = true,
