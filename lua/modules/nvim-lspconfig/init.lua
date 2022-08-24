099187900e4d45ddbd3b9ae49bddf266954020c8
Yudi <madewahyudi22@gmail.com>
2022-08-24 12:07:20 +0800

    update DiagnosticSign icons

--- lua/modules/nvim-lspconfig/init.lua
+++ lua/modules/nvim-lspconfig/init.lua
@@ -1,5 +1,11 @@
 local nmap = require("common.utils").nmap
 
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 require("modules.nvim-lspconfig.lsp.html")
 require("modules.nvim-lspconfig.lsp.jsonls")
 require("modules.nvim-lspconfig.lsp.lua")
