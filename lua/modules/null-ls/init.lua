c8e94fbfa57249d06a5f19fbea2050380175c5d8
Yudi <madewahyudi22@gmail.com>
2022-09-04 20:05:18 +0800

    restart eslint_d server when eslint config changes

--- lua/modules/null-ls/init.lua
+++ lua/modules/null-ls/init.lua
@@ -7,6 +7,19 @@ local diagnostics = null_ls.builtins.diagnostics
 nmap("<leader>ni", "<CMD>NullLsInfo<CR>")
 nmap("<leader>nl", "<CMD>NullLsLog<CR>")
 
+local group = vim.api.nvim_create_augroup("eslint_d_restart", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = group,
+  pattern = {
+    "package.json",
+    "package-lock.json",
+    "*eslint*",
+  },
+  callback = function()
+    vim.cmd("silent exec '!eslint_d restart'")
+  end,
+})
+
 null_ls.setup({
   sources = {
     formatting.eslint_d,
