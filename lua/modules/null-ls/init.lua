36190340c81624a95f3256c2f6ee47aa3f7c6a10
Yudi <madewahyudi22@gmail.com>
2022-09-09 10:44:36 +0800

    minor, add notification when auto restarting the eslint_d

--- lua/modules/null-ls/init.lua
+++ lua/modules/null-ls/init.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     "*eslint*",
   },
   callback = function()
+    vim.notify("restarting eslint_d")
     vim.cmd("silent exec '!eslint_d restart'")
   end,
 })
