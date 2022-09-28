4c6383821aa5794a38ac0938ecec071d447a8d6e
Yudi <madewahyudi22@gmail.com>
2022-09-28 12:33:48 +0800

    update floaterm mapping

--- lua/modules/floaterm/init.lua
+++ lua/modules/floaterm/init.lua
@@ -4,8 +4,8 @@ local nmap = utils.nmap
 vim.g.floaterm_title = "Terminal [$1/$2]"
 vim.g.floaterm_keymap_toggle = "<silent><localleader>T"
 vim.g.floaterm_keymap_new = "<localleader>tn"
-vim.g.floaterm_keymap_prev = "<F8>"
-vim.g.floaterm_keymap_next = "<F9>"
+vim.g.floaterm_keymap_prev = "<A-.>"
+vim.g.floaterm_keymap_next = "<A-,>"
 vim.g.floaterm_keymap_kill = "<localleader>tq"
 vim.g.floaterm_width = 0.9
 vim.g.floaterm_height = 0.9
@@ -21,7 +21,4 @@ nmap("<localleader>tg", "<CMD>FloatermNew --name=lazygit --autoclose=2 lazygit<C
 -- lazydocker
 nmap("<localleader>td", "<CMD>FloatermNew --name=lazydocker --autoclose=2 lazydocker<CR>")
 -- ncdu
-nmap(
-  "<localleader>tu",
-  "<CMD>FloatermNew --name=lazydocker --autoclose=2 ncdu --color dark<CR>"
-)
+nmap("<localleader>tu", "<CMD>FloatermNew --name=lazydocker --autoclose=2 ncdu --color dark<CR>")
