e64006137f7962a070d00055e4e555d61bd6a535
Yudi <madewahyudi22@gmail.com>
2023-02-26 17:28:06 +0800

    add eslint codeaction to null_ls

--- lua/modules/null-ls/init.lua
+++ lua/modules/null-ls/init.lua
@@ -3,6 +3,7 @@ local nmap = utils.nmap
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 nmap("<leader>ni", "<CMD>NullLsInfo<CR>")
 nmap("<leader>nl", "<CMD>NullLsLog<CR>")
@@ -13,5 +14,6 @@ null_ls.setup({
     formatting.stylua,
     diagnostics.zsh,
     formatting.dart_format,
+    code_actions.eslint,
   },
 })
