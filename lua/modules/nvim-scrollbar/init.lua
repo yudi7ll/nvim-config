940c5885a72ab087575ceb3115c578665dca227a
Yudi <madewahyudi22@gmail.com>
2023-10-07 13:29:45 +0800

    update scrollbar color

--- lua/modules/nvim-scrollbar/init.lua
+++ lua/modules/nvim-scrollbar/init.lua
@@ -11,7 +11,7 @@ require("scrollbar").setup({
     blend = 50, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
     color = nil,
     color_nr = nil, -- cterm
-    highlight = "CursorColumn",
+    highlight = "Pmenu",
     hide_if_all_visible = true, -- Hides handle if all lines are visible
   },
   marks = {
