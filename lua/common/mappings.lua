fac28c3c676c49bebf4cac654d021d990eb8dbfc
Yudi <madewahyudi22@gmail.com>
2024-09-07 13:08:21 +0800

    fix shift+space printing weird char

--- lua/common/mappings.lua
+++ lua/common/mappings.lua
@@ -6,6 +6,8 @@ local format = require("lib.format").format
 local nmap = utils.nmap
 local vmap = utils.vmap
 local xmap = utils.xmap
+local imap = utils.imap
+local tmap = utils.tmap
 local formatting = function()
   vim.lsp.buf.format({ async = true })
 end
@@ -19,6 +21,8 @@ nmap("<leader>ls", ":LspSettings local ", { silent = false })
 nmap("<leader>lS", ":LspSettings ", { silent = false })
 nmap("<leader>k", "<CMD>w<CR>")
 xmap("<leader>p", '"_dP')
+imap("<S-Space>", "<Space>", { silent = true })
+tmap("<S-Space>", "<Space>", { silent = true })
 
 -- lsp mapping
 M.attach_mapping = function(bufnr)
